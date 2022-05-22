c63940fe2c4b62f7eef7098860a99b29c1e3f9e8
Nanda <mariafsguedes@fac.pe.senac.br>
2022-05-22 16:02:39 -0300

    ok

--- exer1.py
+++ exer1.py
@@ -1,4 +1,5 @@
-#o float é necessário antes do input porque este sempre retorna uma string
+#Escreva um programa que receba 2 notas de um aluno (float) e informe a média
+
 
 x = float(input("Digite sua primeira nota:"))
 y = float(input("Digite sua segunda nota:"))
@@ -6,3 +7,5 @@ y = float(input("Digite sua segunda nota:"))
 media = (( x + y ) /2)
 
 print("Sua média é:", media)
+
+#o float é necessário antes do input porque este sempre retorna uma string
