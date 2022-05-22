643be83e25a3d92de88d962268d055ffa3a39748
Nanda <mariafsguedes@fac.pe.senac.br>
2022-05-22 20:21:13 -0300

    resolver erro

--- exer21.py
+++ exer21.py
@@ -1,16 +1,17 @@
 #Faça um programa, com uma função que necessite de três argumentos, e que forneça a soma desses três argumentos.
 
-def num(a,b,c):
-    a = int(input("Digite o primeiro número:"))
-    b = int(input("Digite o segundo número:"))
-    c = int(input("Digite o terceiro número:"))
+a = int(input("Digite o primeiro número:"))
+b = int(input("Digite o segundo número:"))
+c = int(input("Digite o terceiro número:"))
 
-    s = (a+b+c)
+
+def soma(a,b,c):
+
+    s  = (a+b+c)
     
-    return s
-   
-    #print("O total dos referidos números é:",s)
+    print("O total dos referidos números é:",soma(a,b,c))
+    #num(a,b,c)
     
     
-    #sum(num)
-    #return sum
+    
+    
