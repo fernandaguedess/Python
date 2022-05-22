f6e273dd97410e648446401f101a5cc3b2c01def
Nanda <mariafsguedes@fac.pe.senac.br>
2022-05-22 15:59:46 -0300

    ultima alteração

--- exer5.py
+++ exer5.py
@@ -1,5 +1,5 @@
 salario = float(input("Digite aqui o seu salário:"))
 
-valor = (((100+15)/100.00)*salario)
+valor = (((100+15)/100)*salario)
 
 print("O dado salário com reajuste de 15% é: R$ ",valor)
