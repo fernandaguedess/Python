d5946874c777f558090e2a32cb217e853ca0f1b1
Nanda <mariafsguedes@fac.pe.senac.br>
2022-05-22 20:09:12 -0300

    tentativa de fazer a primeira questão

--- exer21.py
+++ exer21.py
@@ -1,2 +1,16 @@
 #Faça um programa, com uma função que necessite de três argumentos, e que forneça a soma desses três argumentos.
 
+def num(a,b,c):
+    a = int(input("Digite o primeiro número:"))
+    b = int(input("Digite o segundo número:"))
+    c = int(input("Digite o terceiro número:"))
+
+    s = (a+b+c)
+    
+    return s
+   
+    #print("O total dos referidos números é:",s)
+    
+    
+    #sum(num)
+    #return sum
