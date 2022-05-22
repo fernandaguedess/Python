190228d47f6c54efbe4dfdf242c1265b86853e5f
Nanda <mariafsguedes@fac.pe.senac.br>
2022-05-22 14:41:24 -0300

    prática

--- start.py
+++ start.py
@@ -1,4 +1,5 @@
 if 5 > 2:
   print("Five is greater than two!")
 
-  '''não tinha funcionado no coemço porque eu coloquei um < em vez do sinal correto'''
+  """não tinha funcionado no coemço porque eu coloquei um < em vez do sinal correto"""
+  """multiline comments works both with single and quotation marks"""
